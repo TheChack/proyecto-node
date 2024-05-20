0eb4ae79b7e4e34d6a0b33091ae58de9363d527f
Eduardo210 <victorevivanco@gmail.com>
2024-05-20 20:42:07 -0300

    alt lista de articulos

--- js/lista-productos.js
+++ js/lista-productos.js
@@ -2,10 +2,10 @@ async function gridCreator() {
   const response = await fetch("../assets/json-products/products.json");
   const products = await response.json();
   console.log("esta tomando el script");
-  let grid = products.map((item) => {
+  let grid = products.map((item, index) => {
     let divTitle = `<div class="grid-title">${item.title}</div>`;
     let divPrice = `<div class="grid-price">${item.price}</div>`;
-    let divImage = `<div class="grid-image"><img src="${item.image}"/></div>`;
+    let divImage = `<div class="grid-image"><img alt="grid-${index}" src="${item.image}"/></div>`;
     let div = `<a href="./producto.html"><div class="grid-item">${divImage} ${divTitle} ${divPrice} </div></a>`;
     return div;
   });
